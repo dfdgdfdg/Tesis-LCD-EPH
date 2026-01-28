2d6f8d35d4db581705d13d62ac3fba404c9546b6
Nico_Spisso <nicolasspisso@hotmail.com>
2026-01-28 17:14:29 -0300

    Pusheo deep search + mlp

--- src/encuestador/Feature_Engineering.py
+++ src/encuestador/Feature_Engineering.py
@@ -29,15 +29,6 @@ data['P05'] = data['P05'].map({0:0,1: 1, 2: 0})
 data['P08'] = data['P08'].map({0:0,1: 1, 2:1, 3:3,9:0}) 
 data['P09'] = data['P09'].map({0:0,9:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8}) 
 data['PP07G_59'] = data['PP07G_59'].map({0:0, 5:5,1:0}) 
-
-data = data.astype({'ANO4': 'category', 'TRIMESTRE': 'category', 'AGLOMERADO': 'category', 'V01': 'category', 'H05': 'category', 
-                    'H06': 'category', 'H08': 'category', 'H09':'category','H12':'category', 'PROP':'category', 'H14':'category','H13':'category',
-                    'CAT_INAC':'category', 'CAT_OCUP':'category', 'CH07':'category', 'P10':'category','P05':'category'
-                    ,'PP07G1':'category', 'PP07G2':'category','PP07G3':'category', 'PP07G4':'category','PP07H':'category',
-                    'PP07I':'category', 'PP07J':'category', 'PP07K':'category','Region':'category', 'P08':'category', 'P09':'category','CONDACT':'category','PP07G_59':'category', 'P02':'category'})
-
-
-#Intento reducir registros por categoria, quiero reducir cardinalidad de variables categoricas
 data['V01'] = data['V01'].map({0:0,1:1,2:2,3:0,4:0,5:0,6:0}) 
 data['H05'] = data['H05'].map({0:0,1:1,2:2,3:0,4:0}) 
 data['H06'] = data['H06'].map({0:0,1:1,2:2,3:3,4:4,5:0,6:0,7:0,9:9}) 
@@ -48,8 +39,12 @@ data['H14'] = data['H14'].map({0:0,1:1,2:2,3:3,4:0,5:0})
 data['H13'] = data['H13'].map({0:0,1:1,2:0,4:0})
 data['P10'] = data['P10'].map({0:0,1:1,2:2,9:0})
 #Todas las columnas de arriba son categoricas, las convierto a category
-data = data.astype({'V01': 'category', 'H05': 'category', 'H06': 'category', 'H08': 'category', 
-                    'H12':'category', 'PROP':'category', 'H14':'category','H13':'category','P10':'category'})
+data = data.astype({'ANO4': 'category', 'TRIMESTRE': 'category', 'AGLOMERADO': 'category', 'V01': 'category', 'H05': 'category', 
+                    'H06': 'category', 'H07': 'category', 'H08': 'category', 'H09':'category','H10':'category','H11':'category','H12':'category', 'PROP':'category', 'H14':'category','H13':'category',
+                    'CAT_INAC':'category', 'CAT_OCUP':'category', 'CH07':'category', 'P10':'category','P05':'category', 'P07':'category',
+                    'PP07G1':'category', 'PP07G2':'category','PP07G3':'category', 'PP07G4':'category','PP07H':'category',
+                    'PP07I':'category', 'PP07J':'category', 'PP07K':'category','Region':'category', 'P08':'category', 'P09':'category','CONDACT':'category','PP07G_59':'category', 'P02':'category'})
+
 categorical_columns = data.select_dtypes(include=['category']).columns
 
 #Quiero hacer nuevas columnas "dummy" para ANIO y TRIMESTRE
@@ -93,5 +88,3 @@ data.drop(columns=['Rango_Etario'], inplace=True)
 #Maximo nivel educativo en el hogar
 
 data['Max_Nivel_Educativo'] = data.groupby(['CODUSU'])['P09'].transform(lambda s: s.astype(int).max())
-
-print(data.head(20))
